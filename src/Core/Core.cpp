0d3c51ca81f42f72208198bc7ec005a10b9cf774
Preciado <aarongarpre@gmail.com>
2023-01-29 19:47:56 +0100

    Backbuffer created.

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -3,6 +3,8 @@
 Core::Core(HWND& hwnd) {
 	this->hwnd = hwnd;
 
+	this->nNumBackBuffers = 2;
+
 	UINT dxgiFactoryCreationFlags = 0;
 
 #ifndef NDEBUG
@@ -36,7 +38,7 @@ Core::Core(HWND& hwnd) {
 		ComPtr<IDXGISwapChain1> sc;
 
 		DXGI_SWAP_CHAIN_DESC1 scDesc = { };
-		scDesc.BufferCount = 2;
+		scDesc.BufferCount = this->nNumBackBuffers;
 		scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 		scDesc.SampleDesc.Count = 1;
 		scDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
@@ -49,7 +51,35 @@ Core::Core(HWND& hwnd) {
 
 	ThrowIfFailed(this->dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(this->allocator.GetAddressOf()))); // We create our command allocator
 
-	InitBuffer();
+	/* Creation of our backbuffer */
+	D3D12_DESCRIPTOR_HEAP_DESC backBufferDescriptorHeapDesc = { };
+	backBufferDescriptorHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	backBufferDescriptorHeapDesc.NumDescriptors = this->nNumBackBuffers;
+	backBufferDescriptorHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	
+	ThrowIfFailed(this->dev->CreateDescriptorHeap(&backBufferDescriptorHeapDesc, IID_PPV_ARGS(this->backBufferDescriptorHeap.GetAddressOf())));
+
+	this->backBufferOffset = this->dev->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE backBufferHandle(this->backBufferDescriptorHeap->GetCPUDescriptorHandleForHeapStart());
+	
+	for (int i = 0; i < this->nNumBackBuffers; i++) {
+		ComPtr<ID3D12Resource> backBuffer;
+		ThrowIfFailed(this->sc->GetBuffer(i, IID_PPV_ARGS(backBuffer.GetAddressOf())));
+		this->backBuffers.push_back(backBuffer);
+
+		this->dev->CreateRenderTargetView(this->backBuffers[i].Get(), nullptr, backBufferHandle);
+		backBufferHandle.Offset(1, this->backBufferOffset);
+	}
+	/* End:Creation of our backbuffer */
+
+
+	this->InitBuffer();
+	this->InitPipeline();
+	
+	// Creation of our command list. Our command list requires our pipeline state, so, we create after pipeline initialization
+	ThrowIfFailed(this->dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, this->allocator.Get(), this->plState.Get(), IID_PPV_ARGS(this->list.GetAddressOf())));
+	ThrowIfFailed(this->list->Close());
 
 }
 
@@ -72,10 +102,13 @@ void Core::InitBuffer() {
 	UINT verticesSize = sizeof(vertices); // Size of our vertex array
 
 	/* Creation of our vertex buffer */
+	D3D12_HEAP_PROPERTIES vertexBufferProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	D3D12_RESOURCE_DESC vertexBufferDesc = CD3DX12_RESOURCE_DESC::Buffer(verticesSize);
+
 	ThrowIfFailed(this->dev->CreateCommittedResource(
-		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		&vertexBufferProperties,
 		D3D12_HEAP_FLAG_NONE,
-		&CD3DX12_RESOURCE_DESC::Buffer(verticesSize),
+		&vertexBufferDesc,
 		D3D12_RESOURCE_STATE_GENERIC_READ,
 		nullptr,
 		IID_PPV_ARGS(this->vertexBuffer.GetAddressOf())
@@ -114,6 +147,83 @@ void Core::GetMostCapableAdapter(ComPtr<IDXGIAdapter>& adapter, ComPtr<IDXGIFact
 		MessageBox(this->hwnd, "Your adapter must be at least D3D_FEATURE_LEVEL_11_0", "Error", MB_OK | MB_ICONERROR);
 
 	return;
+}
+
+/*
+	In this method we will initialize our pipeline state
+		Note: Additionally we are going to create an input layout and a root signature.
+*/
+void Core::InitPipeline() {
+	/* Creation of our root signature */
+	D3D12_ROOT_SIGNATURE_DESC rootSigDesc = { };
+	rootSigDesc.pParameters = nullptr;
+	rootSigDesc.pStaticSamplers = nullptr;
+	rootSigDesc.NumParameters = 0;
+	rootSigDesc.NumStaticSamplers = 0;
+	rootSigDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	ComPtr<ID3DBlob> rootSigBlob;
+	ComPtr<ID3DBlob> rootSigErrorBlob;
+	ThrowIfFailed(D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1_0, rootSigBlob.GetAddressOf(), rootSigErrorBlob.GetAddressOf()));
+	
+	if (rootSigErrorBlob) {
+		MessageBox(this->hwnd, (char*)rootSigErrorBlob->GetBufferPointer(), "Error", MB_OK | MB_ICONERROR);
+		return;
+	}
+	this->dev->CreateRootSignature(0, rootSigBlob->GetBufferPointer(), rootSigBlob->GetBufferSize(), IID_PPV_ARGS(this->rootSig.GetAddressOf()));
+	/* End:Creation of our root signature */
+
+	/* Creation of our input layout */
+	D3D12_INPUT_ELEMENT_DESC inputElements[] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, NULL},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, NULL}
+	};
+
+	D3D12_INPUT_LAYOUT_DESC iLayoutDesc = { };
+	iLayoutDesc.NumElements = _countof(inputElements);
+	iLayoutDesc.pInputElementDescs = inputElements;
+	/* End:Creation of our input layout */
+
+	/* Compile our shader */
+	WCHAR fullShaderPath[MAX_PATH];
+	GetFullPathNameW(L"shader.fx", MAX_PATH, fullShaderPath, nullptr);
+
+	ComPtr<ID3DBlob> VertexShader, PixelShader;
+	ComPtr<ID3DBlob> VertexShaderError, PixelShaderError;
+
+	ThrowIfFailed(D3DCompileFromFile(fullShaderPath, nullptr, nullptr, "VertexMain", "vs_5_1", NULL, NULL, VertexShader.GetAddressOf(), VertexShaderError.GetAddressOf()));
+	ThrowIfFailed(D3DCompileFromFile(fullShaderPath, nullptr, nullptr, "PixelMain", "ps_5_1", NULL, NULL, PixelShader.GetAddressOf(), PixelShaderError.GetAddressOf()));
+	
+	if (VertexShaderError) {
+		MessageBox(this->hwnd, (char*)VertexShaderError->GetBufferPointer(), "Error", MB_OK | MB_ICONERROR);
+		return;
+	}
+	
+	if (PixelShaderError) {
+		MessageBox(this->hwnd, (char*)PixelShaderError->GetBufferPointer(), "Error", MB_OK | MB_ICONERROR);
+		return;
+	}
+	/* End:Compile our shader */
+
+	/* Creation of our pipeline state */
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC plDesc = { };
+	plDesc.VS = CD3DX12_SHADER_BYTECODE(VertexShader.Get());
+	plDesc.PS = CD3DX12_SHADER_BYTECODE(PixelShader.Get());
+	plDesc.InputLayout = iLayoutDesc;
+	plDesc.pRootSignature = this->rootSig.Get();
+	plDesc.Flags = D3D12_PIPELINE_STATE_FLAG_NONE;
+	plDesc.RTVFormats[0] = DXGI_FORMAT_B8G8R8A8_UNORM;
+	plDesc.NumRenderTargets = 1;
+	plDesc.DepthStencilState.DepthEnable = FALSE; // Enable this 2 options if you're going to use Depth Stencil.s
+	plDesc.DepthStencilState.StencilEnable = FALSE;
+	plDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	plDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+	plDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+	plDesc.SampleMask = UINT32_MAX;
+	plDesc.SampleDesc.Count = 1;
+
+	ThrowIfFailed(this->dev->CreateGraphicsPipelineState(&plDesc, IID_PPV_ARGS(this->plState.GetAddressOf())));
+	/* End:Creation of our pipeline state*/
 }
 
 /*
--- src/Core/Core.h
+++ src/Core/Core.h
@@ -6,6 +6,7 @@
 #include <DirectX/d3dx12.h>
 #include <dxgi1_4.h>
 #include <vector>
+#include <d3dcompiler.h>
 
 using namespace Microsoft::WRL;
 typedef float RGB[3];
@@ -36,18 +37,30 @@ private:
 	ComPtr<ID3D12GraphicsCommandList> list;
 	ComPtr<ID3D12CommandQueue> queue;
 	ComPtr<ID3D12CommandAllocator> allocator;
+
+	ComPtr<ID3D12PipelineState> plState;
 	
+	ComPtr<ID3D12DescriptorHeap> backBufferDescriptorHeap;
+	
+	ComPtr<ID3D12RootSignature> rootSig;
+
 	ComPtr<ID3D12Fence> fence;
 
 	ComPtr<ID3D12Resource> vertexBuffer;
 	D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
 
+	std::vector<ComPtr<ID3D12Resource>> backBuffers;
+
+	UINT backBufferOffset;
+
+	int nNumBackBuffers;
+	
 	void GetMostCapableAdapter(ComPtr<IDXGIAdapter>& adapter, ComPtr<IDXGIFactory2>& factory);
 
 	D3D_FEATURE_LEVEL GetAdapterMaxFeatureLevel(ComPtr<IDXGIAdapter>& adapter);
 
 	void InitBuffer();
-
+	void InitPipeline();
 public:
 	Core(HWND& hwnd);
 };
